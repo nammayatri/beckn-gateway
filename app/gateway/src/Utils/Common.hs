06bea1cdf25afd7e451ffd474121c44d107be259
Utkarsh <utkarsh.pandey1@juspay.in>
2023-02-22 14:46:02 +0530

    add_license

--- app/gateway/src/Utils/Common.hs
+++ app/gateway/src/Utils/Common.hs
@@ -1,3 +1,13 @@
+ {-
+ This is the default license template.
+ 
+ File: Common.hs
+ Author: utkarshpandey
+ Copyright (c) 2023 utkarshpandey
+ 
+ To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+-}
+
 module Utils.Common
   ( module CoreCommon,
   )
--- app/mock-registry/src/Storage/Queries/Subscriber.hs
+++ app/mock-registry/src/Storage/Queries/Subscriber.hs
@@ -1,3 +1,13 @@
+ {-
+ This is the default license template.
+ 
+ File: Subscriber.hs
+ Author: utkarshpandey
+ Copyright (c) 2023 utkarshpandey
+ 
+ To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+-}
+
 module Storage.Queries.Subscriber where
 
 import Kernel.Prelude
