c32898963fdf1957b494dc5953ca851e039c32c9
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-02-27 14:06:13 -0500

    Update Subscriber.hs

--- app/mock-registry/src/Storage/Tabular/Subscriber.hs
+++ app/mock-registry/src/Storage/Tabular/Subscriber.hs
@@ -21,10 +21,10 @@
 
 module Storage.Tabular.Subscriber where
 
+import qualified Domain.Subscriber as Domain
 import Kernel.Prelude
 import Kernel.Storage.Esqueleto
 import Kernel.Types.Base64
-import qualified Kernel.Types.Registry.Subscriber as Domain
 
 derivePersistField "Domain.Domain"
 
