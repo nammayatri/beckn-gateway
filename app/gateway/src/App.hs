4558349c7528461d851b334295a5dcecb60e88cd
Rohit Saini <rohitsaini@Rohits-MacBook-Pro.local>
2026-05-18 18:59:41 +0530

    becknGateway - Make Service Port and put dhall as a fallback

--- app/gateway/src/App.hs
+++ app/gateway/src/App.hs
@@ -42,7 +42,8 @@ import Utils.Common
 
 runGateway :: (AppCfg -> AppCfg) -> IO ()
 runGateway configModifier = do
-  appCfg <- configModifier <$> readDhallConfigDefault "beckn-gateway"
+  appCfg' <- configModifier <$> readDhallConfigDefault "beckn-gateway"
+  appCfg <- overridePortFromEnv appCfg'
   let port = appCfg.port
   let metricsPort = appCfg.metricsPort
   Metrics.serve metricsPort
@@ -64,3 +65,10 @@ runGateway configModifier = do
         logInfo ("Runtime created. Starting server at port " <> show port)
         return flowRt'
     runSettings settings $ run (App.EnvR flowRt' appEnv)
+
+overridePortFromEnv :: AppCfg -> IO AppCfg
+overridePortFromEnv cfg = do
+  mbPort <- lookupEnv "SERVICE_PORT"
+  pure $ case mbPort >>= readMaybe of
+    Just p -> cfg {port = p}
+    Nothing -> cfg
